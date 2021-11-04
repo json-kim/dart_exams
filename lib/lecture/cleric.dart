31058d1972b983cc173d121db5662de0cc2e969f
json-kim <flutter_dart@kakao.com>
2021-11-04 12:19:18 +0900

    feat(cleric.dart): 'cleric' 클래스 'pray' 메서드 추

--- lib/lecture/cleric.dart
+++ lib/lecture/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   String name;
   int hp;
@@ -17,12 +19,20 @@ class Cleric {
     hp = maxHp;
   }
 
+  int pray(int sec) {
+    int curMp = mp;
+    Random random = Random();
+    mp = min(sec + Random().nextInt(3) + mp, maxMp);
+
+    // 실제로 회복된 mp 양 리턴
+    return mp - curMp;
+  }
+
   String status() {
     return '''
-  name: $name
-  hp: $hp
-  mp: $mp
-  ''';
+name: $name
+hp: $hp
+mp: $mp''';
   }
 }
 
@@ -33,14 +43,18 @@ void main() {
 
   print(cleric1.status());
 
-  cleric1.selfAid();
+  print('1번째 mp 회복량 : ${cleric1.pray(3)}');
 
   print(cleric1.status());
 
-  print(cleric2.status());
+  print('2번째 mp 회복량 : ${cleric1.pray(4)}');
 
-  cleric2.selfAid();
+  print(cleric1.status());
 
-  print(cleric2.status());
+  print('3번째 mp 회복량 : ${cleric1.pray(5)}');
+
+  print(cleric1.status());
+
+
 
 }
