fe39e43de85f944aa36ae42d71ca78d02f4dbba5
json-kim <flutter_dart@kakao.com>
2021-11-05 11:05:18 +0900

    refactor: 'Cleric' 클래스에 static 추가, 생성자 기본값으로 이용

--- lib/lecture/cleric.dart
+++ lib/lecture/cleric.dart
@@ -1,15 +1,18 @@
 import 'dart:math';
 
 class Cleric {
+  // Field (Property)
   String name;
   int hp;
   int mp;
 
-  int maxHp = 50;
-  int maxMp = 10;
+  static const int maxHp = 50;
+  static const int maxMp = 10;
 
-  Cleric({this.name, this.hp, this.mp});
+  // Constructor
+  Cleric(this.name, {this.hp = maxHp, this.mp = maxMp});
 
+  // Method
   void selfAid() {
     if (mp < 5) {
       return;
@@ -21,6 +24,11 @@ class Cleric {
 
   int pray(int sec) {
     int curMp = mp;
+
+    // 빈번하게 사용하는 함수라면 Random객체를 따로 만들어서 사용,
+    // 그러나 현실과의 괴리감 (성직자의 속성으로 랜덤 객체를 가지고 있는 것이 맞는가?)
+    // '_' 를 사용하면 private 속성이 되어 감출 수 있다.
+    // 마나 회복량 : 기도한 초 + 보정 포인트 + 기존 마나
     mp = min(sec + Random().nextInt(3) + mp, maxMp);
 
     // 실제로 회복된 mp 양 리턴
@@ -36,9 +44,9 @@ mp: $mp''';
 }
 
 void main() {
-  Cleric cleric1 = Cleric(name: '초보자', hp: 10, mp: 1);
-  Cleric cleric2 = Cleric(name: '중수', hp: 30, mp: 5);
-  Cleric cleric3 = Cleric(name: '고수', hp: 50, mp: 10);
+  Cleric cleric1 = Cleric('초보자', hp: 10, mp: 1);
+  Cleric cleric2 = Cleric('중수', hp: 30, mp: 5);
+  Cleric cleric3 = Cleric('고수', hp: 50, mp: 10);
 
   print(cleric1.status());
 
