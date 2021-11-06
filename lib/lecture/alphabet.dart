adc5a16421b2f9eca7bd1fd3b2a5e5ca80a991f6
json-kim <flutter_dart@kakao.com>
2021-11-06 15:23:15 +0900

    refactor: 'Alphabet' 클래스 메서드 수정

--- lib/lecture/alphabet.dart
+++ lib/lecture/alphabet.dart
@@ -2,17 +2,18 @@ import 'dart:math';
 
 void main() {
   Random random = Random();
+  Alphabet alphabet = Alphabet();
 
   for (int i = 0; i < 10; i++) {
     String alpha = Alphabet.alphabet[random.nextInt(27)];
-    print('$alpha : ${Alphabet.getIndexOfAlphabet(alpha)}');
+    print('$alpha : ${alphabet.getIndexOfAlphabet(alpha)}');
   }
 }
 
 class Alphabet {
   static const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  static int getIndexOfAlphabet(String alpha) {
+  int getIndexOfAlphabet(String alpha) {
     // 입력 알파벳이 몇번쨰 알파벳인지 나타내는 정수값을 리턴
     return alphabet.indexOf(alpha) + 1;
   }
